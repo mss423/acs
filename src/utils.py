b6f53e64bef20e1c28d7924708fd2cf6a2e89420
Max Springer <mss423@cornell.edu>
2025-05-12 20:25:12 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -156,7 +156,7 @@ def binary_similarity_search(data, num_samples, coverage, max_degree=None, epsil
         sim = (sim_upper + sim_lower) / 2
         # DEBUG
         print("  ! ============== DEBUG ============== ! ")
-        print(f" ! == Coverage = {current_coverage} == !")
+        print(f" ! == # Samples = {len(samples)} == !")
 
     print(f"Completed with similarity threshold = {sim/1000}")
     return sim / 1000, node_graph, samples, current_coverage
